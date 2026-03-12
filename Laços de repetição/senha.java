5515ea5fa3575dd35993e389e9664d64f4383cc6
Jonas Mamede <mamedej777@gmail.com>
2026-03-12 18:46:56 -0300

    Update senha.java

--- Laços de repetição/senha.java
+++ Laços de repetição/senha.java
@@ -1,11 +1,23 @@
+
 import java.util.Scanner;
 
 public class senha {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        char senha = 'jonas123';
+        String senhacorreta = "1234";
 
-        
+        while (true) {
+            System.out.println("Digite a senha: ");
+            String digitado = entrada.nextLine();
+
+            if (digitado.equals(senhacorreta)) {
+                System.out.println("Acesso Permitido");
+                break;
+            } else {
+                System.out.println("Acesso negado");
+
+            }
+        }
     }
 }
