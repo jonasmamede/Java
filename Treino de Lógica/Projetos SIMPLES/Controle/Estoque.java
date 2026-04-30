d54d731592f6189ed63b292efea11b81f6f34661
Jonas Mamede <mamedej777@gmail.com>
2026-04-30 20:37:48 -0300

    Projeto - Controle de estoque
    Continuação do projeto de controle de estoque.

--- Treino de Lógica/Projetos SIMPLES/Controle/Estoque.java
+++ Treino de Lógica/Projetos SIMPLES/Controle/Estoque.java
@@ -1,8 +1,34 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Estoque {
     ArrayList <Produtos>produtosList = new ArrayList<>();
-    public void adicionarProduto(Produtos p){
-        produtosList.add(p);
+    Scanner entrada = new Scanner(System.in);
+    public void adicionarProduto(int quant){
+        for (int i = 0; i < quant; i++) {
+            Produtos prod = new Produtos();
+            System.out.println("Código " + (i+1) + ": ");
+            prod.Codigo = entrada.nextInt();
+            entrada.nextLine();
+
+            System.out.println("");
+            System.out.println("Produto " + (i+1) + ": ");
+            prod.Nome = entrada.nextLine();
+            System.out.println("");
+
+            System.out.println("Quantidade " + (i+1) + ": ");
+            prod.Quantidade = entrada.nextInt();
+            produtosList.add(prod);
+            System.out.println("");
+            System.out.println("Preço Unitário" + (i+1) + ": ");
+            prod.Preco = entrada.nextDouble();
+            System.out.println("---------------------------");
+        }
+    }
+    public void listarProdutos(){
+        for (int i = 0; i < produtosList.size(); i++) {
+            Produtos p = produtosList.get(i);
+            System.out.println((i+1) + " - "+ p.Codigo + " // " + p.Nome + " // Quantidade - " + p.Quantidade + "x // " + p.Preco + "$");
+        }
     }
 }
--- Treino de Lógica/Projetos SIMPLES/Controle/Main.java
+++ Treino de Lógica/Projetos SIMPLES/Controle/Main.java
@@ -1,15 +1,15 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Estoque estoque = new Estoque();
-        System.out.println("Nome do produto: ");
-        String nome = entrada.nextLine();
-        Produtos p = new Produtos(nome);
-        Produtos Nome = nome;
-        estoque.adicionarProduto(p);
+        System.out.println("Quantidade de produtos que deseja cadastrar: ");
+        int quant = entrada.nextInt();
+        estoque.adicionarProduto(quant);
+        System.out.println("-----------------");
+        estoque.listarProdutos();
+        
     }
 
 }
