0ae7d6d8dbf2e87b20d5c5f6b65aa2a8bf46bdcd
Jonas Mamede <mamedej777@gmail.com>
2026-05-06 08:03:04 -0300

    Atualizar o Pessoa.java

--- Treino de Lógica/Exercicios Escola/Polimorfismo/Pessoa 2/Pessoa.java
+++ Treino de Lógica/Exercicios Escola/Polimorfismo/Pessoa 2/Pessoa.java
@@ -1,14 +1,14 @@
 class Pessoa {
 
     public void saudacao(Pessoa pessoa) {
-        System.out.println("Olá, pessoa!");
+        return "Olá, pessoa!";
     }
 
     public void saudacao(Aluno aluno) {
-        System.out.println("Olá, aluno!");
+        return "Olá, aluno!";
     }
 
     public void saudacao(Professor professor) {
-        System.out.println("Bom dia, professor!");
+        return "Bom dia, professor!"
     }
 }
