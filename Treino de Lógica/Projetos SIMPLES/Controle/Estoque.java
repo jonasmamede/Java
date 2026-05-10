dc2164356de4ae72d05e72d6b83b45434cc2ef80
Jonas Mamede <mamedej777@gmail.com>
2026-05-10 12:24:12 -0300

    Finalização do projeto de Controle de Estoque
    Depois de semanas, terminei meu primeiro projeto que eu realmente tive que usar tudo que aprendi ate agora.

--- Treino de Lógica/Projetos SIMPLES/Controle/Estoque.java
+++ Treino de Lógica/Projetos SIMPLES/Controle/Estoque.java
@@ -2,44 +2,65 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Estoque {
-    ArrayList <Produtos>produtosList = new ArrayList<>();
+    ArrayList<Produtos> produtosList = new ArrayList<>();
     Scanner entrada = new Scanner(System.in);
-    public void adicionarProduto(int quant){
+
+    public void adicionarProduto(int quant) {
         for (int i = 0; i < quant; i++) {
             Produtos prod = new Produtos();
-            System.out.println("Código " + (i+1) + ": ");
+            System.out.println("Código " + (i + 1) + ": ");
             prod.Codigo = entrada.nextInt();
             entrada.nextLine();
 
             System.out.println("");
-            System.out.println("Produto " + (i+1) + ": ");
+            System.out.println("Produto " + (i + 1) + ": ");
             prod.Nome = entrada.nextLine();
             System.out.println("");
 
-            System.out.println("Quantidade " + (i+1) + ": ");
+            System.out.println("Quantidade " + (i + 1) + ": ");
             prod.Quantidade = entrada.nextInt();
             produtosList.add(prod);
             System.out.println("");
-            System.out.println("Preço Unitário " + (i+1) + ": ");
+            System.out.println("Preço Unitário " + (i + 1) + ": ");
             prod.Preco = entrada.nextDouble();
             System.out.println("---------------------------");
         }
     }
-    public void listarProdutos(){
+
+    public void listarProdutos() {
         for (int i = 0; i < produtosList.size(); i++) {
             Produtos p = produtosList.get(i);
-            System.out.println((i+1) + " - "+ p.Codigo + " // " + p.Nome + " // Quantidade - " + p.Quantidade + "x // " + p.Preco + "$");
+            System.out.println((i + 1) + " - " + p.Codigo + " // " + p.Nome + " // Quantidade - " + p.Quantidade
+                    + "x // " + p.Preco + "$");
         }
     }
 
-
-    public boolean deletarProduto( int codigo){
+    public boolean deletarProduto(int codigo) {
         for (int i = 0; i < produtosList.size(); i++) {
             if (produtosList.get(i).Codigo == codigo) {
                 produtosList.remove(i);
                 return true;
-            }     
-        }  
-        return false; 
+            }
+        }
+        return false;
+    }
+
+    public boolean updateProduto(int codigo2) {
+        for (int i = 0; i < produtosList.size(); i++) {
+            Produtos p = produtosList.get(i);
+            if (p.Codigo == codigo2) {
+                System.out.println("Novo nome: ");
+                entrada.nextLine();
+                p.Nome = entrada.nextLine();
+
+                System.out.println("Nova quantidade: ");
+                p.Quantidade = entrada.nextInt();
+                
+                System.out.println("Novo preço: ");
+                p.Preco = entrada.nextDouble();
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Treino de Lógica/Projetos SIMPLES/Controle/Main.java
+++ Treino de Lógica/Projetos SIMPLES/Controle/Main.java
@@ -4,7 +4,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Estoque estoque = new Estoque();
-        System.out.println("Quantidade de produtos que deseja cadastrar: ");
+        while(true) {
+        
+        System.out.println("-----------------");
+        System.out.println("Bem vindo ao controle de estoque");
+        System.out.println("1 - Adicionar produtos");
+        System.out.println("2 - Listar produtos");
+        System.out.println("3 - Deletar produtos");
+        System.out.println("4 - Atualizar produtos");
+        System.out.println("5 - Encerrar programa");
+        System.out.println("-----------------'");
+        System.out.println("Escolha: ");
+        int escolha = entrada.nextInt();
+        if(escolha == 5){
+            System.out.println("Programa encerrado");
+            break;
+        }
+        switch(escolha){
+            case 1:
+                System.out.println();
+                System.out.println("Quantidade de produtos que deseja cadastrar: ");
+                int quant = entrada.nextInt();
+                estoque.adicionarProduto(quant);
+                break;
+            case 2:
+                System.out.println();
+                System.out.println("------------------------");
+                estoque.listarProdutos();
+                break;
+            case 3:
+                System.out.println();
+                System.out.println("Codigo: ");
+                int codigo = entrada.nextInt();
+                estoque.deletarProduto(codigo);
+                break;
+            case 4:
+                System.out.println();
+                System.out.println("Codigo: ");
+                int codigo2 = entrada.nextInt();
+                estoque.updateProduto(codigo2);
+            default:
+                System.out.println("Digite uma opção válida");
+        }}
+
+        /* System.out.println("Quantidade de produtos que deseja cadastrar: ");
         int quant = entrada.nextInt();
         estoque.adicionarProduto(quant);
 
@@ -17,6 +60,12 @@ public class Main {
         
         System.out.println("-----------------");
         estoque.listarProdutos();
+
+        System.out.println("---------------------");
+        System.out.println("Codigo: ");
+        int codigo2 = entrada.nextInt();
+        estoque.updateProduto(codigo);
+        estoque.listarProdutos(); */
     }
 
 }
